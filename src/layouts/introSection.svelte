f323bc9157fb17f552afbc4f6a79fb7b809561ba
Mohammad Ahmad <mohammad.ahmad@carbonteq.com>
2024-07-22 10:58:48 -0400

    dded fonts and other setup

---
+++ src/layouts/introSection.svelte
@@ -0,0 +1,14 @@
+<div>
+	<h1 class="intro-greeting">Hello,</h1>
+	<h2 class="intro-name">I am Mohammad Ahmad!</h2>
+</div>
+
+<style>
+	.intro-greeting {
+		margin: 0px 0px;
+	}
+
+	.intro-name {
+		margin: 0px 0px;
+	}
+</style>
