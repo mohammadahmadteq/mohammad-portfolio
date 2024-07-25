3077455b40e1636e910f273b2f19edd4108c9ca2
Mohammad Ahmad <mohammad.ahmad@carbonteq.com>
2024-07-25 10:38:06 -0400

    progrress on intro section with logo animations for skilkls

--- src/layouts/introSection.svelte
+++ src/layouts/introSection.svelte
@@ -1,14 +1,102 @@
-<div>
-	<h1 class="intro-greeting">Hello,</h1>
-	<h2 class="intro-name">I am Mohammad Ahmad!</h2>
+<script>
+	import JavascriptLogo from '@components/introLogos/javascriptLogo.svelte';
+	import ReactLogo from '@components/introLogos/reactLogo.svelte';
+	import RustLogo from '@components/introLogos/rustLogo.svelte';
+	import SvelteLogo from '@components/introLogos/svelteLogo.svelte';
+	import TypescriptLogo from '@components/introLogos/typescriptLogo.svelte';
+	import NestjsLogo from '@components/introLogos/nestjsLogo.svelte';
+	import NextjsLogo from '@components/introLogos/nextjsLogo.svelte';
+	import PythonLogo from '@components/introLogos/pythoLogo.svelte';
+	import NodejsLogo from '@components/introLogos/nodejsLogo.svelte';
+	import BunLogo from '@components/introLogos/bunLogo.svelte';
+	import { Tooltip } from '@svelte-plugins/tooltips';
+</script>
+
+<div class="intro-padding">
+	<div class="intro-section">
+		<div>
+			<h1 class="remove-margin">Hello,</h1>
+			<h2 class="remove-margin">I am Mohammad Ahmad!</h2>
+			<h3 class="remove-margin">Software Engineer</h3>
+			<p class="remove-margin">Skills</p>
+		</div>
+		<div class="wave-picture">👋</div>
+	</div>
+
+	<div class="logo-container">
+		<Tooltip style={{ backgroundColor: 'white', color: 'black' }} arrow={false} content="Javascript"
+			><div class="logo"><JavascriptLogo /></div></Tooltip
+		>
+		<Tooltip
+			style={{ backgroundColor: 'white', color: 'black' }}
+			arrow={false}
+			content="Typescript"
+		>
+			<div class="logo"><TypescriptLogo /></div>
+		</Tooltip>
+		<Tooltip style={{ backgroundColor: 'white', color: 'black' }} arrow={false} content="React">
+			<div class="logo"><ReactLogo /></div>
+		</Tooltip>
+		<Tooltip style={{ backgroundColor: 'white', color: 'black' }} arrow={false} content="Svelte">
+			<div class="logo"><SvelteLogo /></div>
+		</Tooltip>
+		<Tooltip style={{ backgroundColor: 'white', color: 'black' }} arrow={false} content="Rust">
+			<div class="logo"><RustLogo /></div>
+		</Tooltip>
+		<Tooltip style={{ backgroundColor: 'white', color: 'black' }} arrow={false} content="Nest.js">
+			<div class="logo"><NestjsLogo /></div>
+		</Tooltip>
+		<Tooltip style={{ backgroundColor: 'white', color: 'black' }} arrow={false} content="Next.js"
+			><div class="logo"><NextjsLogo /></div></Tooltip
+		>
+		<Tooltip style={{ backgroundColor: 'white', color: 'black' }} arrow={false} content="Python"
+			><div class="logo"><PythonLogo /></div></Tooltip
+		>
+		<Tooltip style={{ backgroundColor: 'white', color: 'black' }} arrow={false} content="Nodejs">
+			<div class="logo logo-node"><NodejsLogo /></div></Tooltip
+		>
+
+		<Tooltip style={{ backgroundColor: 'white', color: 'black' }} arrow={false} content="Bun">
+			<div class="logo"><BunLogo /></div>
+		</Tooltip>
+	</div>
 </div>
 
 <style>
-	.intro-greeting {
+	.intro-padding {
+		padding-inline: 10rem;
+	}
+	.intro-section {
+		display: flex;
+		align-items: center;
+	}
+	.remove-margin {
 		margin: 0px 0px;
 	}
-
-	.intro-name {
-		margin: 0px 0px;
+	.wave-picture {
+		font-size: 20rem;
+	}
+	.logo-container {
+		display: flex;
+		width: 100%;
+		column-gap: 0.75rem;
+		margin-top: 1rem;
+	}
+	.logo {
+		max-width: 3rem;
+		height: 3rem;
+		transition: all 0.1s cubic-bezier(0.445, 0.05, 0.55, 0.95);
+	}
+	.logo-node {
+		max-width: 7.3rem;
+		height: 3rem;
+	}
+	.logo:hover {
+		max-width: 6rem;
+		height: 6rem;
+	}
+	.logo-node:hover {
+		max-width: 14.6rem;
+		height: 6rem;
 	}
 </style>
