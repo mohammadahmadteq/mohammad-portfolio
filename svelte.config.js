0c5eeb5a684cbfa2f73e29fa76c53ffd1131b136
Mohammad Ahmad <mohammad.ahmad@carbonteq.com>
2024-11-09 21:55:19 +0500

    config

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -16,6 +16,9 @@ const config = {
 			'@components/*': 'src/components/*',
 			'@styles/*': 'src/styles/*',
 			'@layouts/*': 'src/layouts/*'
+		},
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : ''
 		}
 	}
 };
